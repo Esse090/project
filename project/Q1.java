792eddbe1bf1852a89f2bf5a5b50e27fb6c7a9e8
Esse090 <issesuleiman21@gmail.com>
2024-09-10 07:17:20 -0700

    Add files via upload

---
+++ project/Q1.java
@@ -0,0 +1,15 @@
+public class Q1 {
+    int a = 15;
+    static float b = 25;
+
+    public static void main(String[] args) {
+        int c = 50;
+        Q1 a = new Q1();
+
+        System.out.println(a.a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println();
+    }
+
+}
